fc7e62a4ce433190724cd63afe098e8d667ab5b2
fischerpj <fischer.pj@gmail.com>
2026-08-08 14:03:14 +0000

    2632

---
+++ export.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Create the target parent directory if it doesn't exist
+TARGET="$HOME/EXT/_myIT/_DOCKER/_BUNH"
+echo $TARGET
+#mkdir -p $TARGET
+
+# Copy the /docs folder to _sites/docs
+# -r enables recursive copying for directories
+# -v enables verbose mode to show files being copied
+SOURCE="docs"
+cp -r -v "$SOURCE" "$TARGET"
+
+echo "Successfully copied $SOURCE to $TARGET"
